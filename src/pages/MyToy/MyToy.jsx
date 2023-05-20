222336ce33a1a8288455d2e968893e1f5e1ab101
IgMominKhan <mominkhan8991@gmail.com>
2023-05-20 19:06:19 +0300

    delete my item is in process

--- src/pages/MyToy/MyToy.jsx
+++ src/pages/MyToy/MyToy.jsx
@@ -2,10 +2,11 @@ import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Providers/AuthProvider";
 import { Link } from "react-router-dom";
 import { Button } from "flowbite-react";
+import Swal from "sweetalert2";
 
 const MyToy = () => {
-  const {user} = useContext(AuthContext);
-  
+  const { user } = useContext(AuthContext);
+
   const [myToys, setMyToys] = useState([]);
 
   useEffect(() => {
@@ -13,7 +14,23 @@ const MyToy = () => {
       .then((res) => res.json())
       .then((toys) => setMyToys(toys))
       .catch((err) => console.error(err));
-  }, []);
+  }, [user]);
+
+  // handle delete
+  const handleDelete = (_id) => {
+    console.log(_id);
+    Swal.fire({
+      title: "Do you want to delete the item?",
+      showDenyButton: true,
+      confirmButtonText: `Yes`,
+      denyButtonText: `No`,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        Swal.fire("Deleted!", "", "success");
+      }
+    });
+  };
 
   return (
     <section className="mt-4">
@@ -40,13 +57,19 @@ const MyToy = () => {
               <th scope="col" className="px-6 py-3">
                 Action
               </th>
+              <th scope="col" className="px-6 py-3">
+                Action
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
+              </th>
             </tr>
           </thead>
           <tbody>
             {/* all toys */}
             {myToys.map((toy) => (
               <tr className="bg-white border-b hover:bg-gray-50 font-semibold text-gray-900">
-                <td className="w-32 p-4">
+                <td className="w-32 p-4 !min-w-28 min-w-[5rem]">
                   <img
                     className="max-h-20 w-full"
                     src={toy?.img}
@@ -65,13 +88,26 @@ const MyToy = () => {
                   ${toy?.price}
                 </td>
                 <td className="px-6 py-4">
+                  <Button gradientDuoTone="cyanToBlue">
+                    Update
+                  </Button>
+                </td>
+                <td className="px-6 py-4">
                   <Link
                     to={`/toys/${toy._id}`}
                   >
-                    <Button>
+                    <Button gradientDuoTone="cyanToBlue">
                       View Details
                     </Button>
                   </Link>
+                </td>
+                <td className="px-6 py-4">
+                  <Button
+                    onClick={() => handleDelete(toy?._id)}
+                    gradientDuoTone="pinkToOrange"
+                  >
+                    Delete
+                  </Button>
                 </td>
               </tr>
             ))}
